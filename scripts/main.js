955ba703c21d501a1b0ce5d74181cade1f7b16d9
Thomas <thomasyao15@gmail.com>
2021-07-27 20:25:24 +1000

    Added toggle rainbow functionality

--- scripts/main.js
+++ scripts/main.js
@@ -27,22 +27,53 @@ function addEventListeners() {
 
 function fillSquareColour(e) {
     e.target.classList.add("filled");
+
+    if (rainbowEnabled) {
+        randomlyGeneratedColor = getRandomColor()
+        console.log(randomlyGeneratedColor);
+        e.target.style.cssText = `background-color: ${randomlyGeneratedColor}`;
+    }
 }
+
+function getRandomColor() {
+    var letters = '0123456789ABCDEF';
+    var color = '#';
+    for (var i = 0; i < 6; i++) {
+      color += letters[Math.floor(Math.random() * 16)];
+    }
+    return color;
+  }
 
 function modifyGridSize() {
     gridSize = parseInt(prompt("Enter a custom side length:"));
     
+    if (gridSize > 100) {
+        gridSize = 100;
+    }
     generateGrid(gridSize);
 }
 
 // Set initial grid size
 generateGrid(4);
+let rainbowEnabled = false;
+// let mouseDown = 0;
+
+// document.body.onmousedown = function() { 
+//   ++mouseDown;
+//   console.log(mouseDown);
+// }
+// document.body.onmouseup = function() {
+//   --mouseDown;
+//   console.log(mouseUp);
+// }
 
 
 const modButton = document.getElementById("modify-size");
 const rainbowButton = document.getElementById("toggle-rainbow");
 
 modButton.addEventListener("click", modifyGridSize);
-
+rainbowButton.addEventListener("click", () => {
+    rainbowEnabled = !rainbowEnabled;
+})
 
 
