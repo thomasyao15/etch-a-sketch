884618f6a0500d4ca00eab73c6671aba3c8ce1c1
Thomas <thomasyao15@gmail.com>
2021-07-27 20:13:27 +1000

    Added modify grid size functionality

--- scripts/main.js
+++ scripts/main.js
@@ -3,6 +3,8 @@ function generateGrid(gridSize) {
     const grid = document.getElementById("grid");
     grid.style.cssText = `grid-template-columns: repeat(${gridSize}, 1fr); grid-template-rows: repeat(${gridSize}, 1fr)`;
 
+    grid.innerHTML = '';  // clears all divs before appending
+
     let amountOfSquares = gridSize * gridSize;
 
     for (let i = 0; i < amountOfSquares; i++) {
@@ -27,5 +29,20 @@ function fillSquareColour(e) {
     e.target.classList.add("filled");
 }
 
+function modifyGridSize() {
+    gridSize = parseInt(prompt("Enter a custom side length:"));
+    
+    generateGrid(gridSize);
+}
+
+// Set initial grid size
 generateGrid(4);
+
+
+const modButton = document.getElementById("modify-size");
+const rainbowButton = document.getElementById("toggle-rainbow");
+
+modButton.addEventListener("click", modifyGridSize);
+
+
 
